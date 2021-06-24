70bc0a8f6c2554dd05e272d369a3612e16cd3599
Kitchannel2010 <84881541+Kitchannel2010@users.noreply.github.com>
2021-06-25 07:22:08 +0800

    Add files via upload

---
+++ Login/JavaScript.js
@@ -0,0 +1,13 @@
+﻿function validate() {
+    {
+        var username = document.getElementById("user");
+        var password = document.getElementById("pass");
+
+        //Username and password
+        if (username.value == "" && password.value == "")
+            alert("Username or password can't be blank")
+    }
+    {//Login
+        var _0xa6a4=['129587HlbDeV','value','Username\x20or\x20password\x20is\x20not\x20correct','admin','1nVSrZk','Welcome\x20Bing\x20Sun\x20Li','Welcome\x20Chi\x20Ling\x20Hu','566704acycIj','location','2pLDiuy','32404QNDLDG','917283AxOWkq','success','20443SHKskX','4moLShA','114VwhkxW','Login\x20successfully!','user777888','7RsrhRT','1sMGCjh','(sunny1i59479@)','then','195519CiEmtJ','Welcome\x20admin','210706MxHXOh'];var _0x238cb0=_0x4abf;(function(_0x3a33e5,_0x5084c0){var _0x334be7=_0x4abf;while(!![]){try{var _0x599e8d=-parseInt(_0x334be7(0x113))*-parseInt(_0x334be7(0x11a))+-parseInt(_0x334be7(0x117))*parseInt(_0x334be7(0x10f))+-parseInt(_0x334be7(0x10a))*parseInt(_0x334be7(0x119))+-parseInt(_0x334be7(0x108))+parseInt(_0x334be7(0x105))*-parseInt(_0x334be7(0x10c))+-parseInt(_0x334be7(0x10b))*-parseInt(_0x334be7(0x114))+parseInt(_0x334be7(0x110))*parseInt(_0x334be7(0x10e));if(_0x599e8d===_0x5084c0)break;else _0x3a33e5['push'](_0x3a33e5['shift']());}catch(_0x4bf690){_0x3a33e5['push'](_0x3a33e5['shift']());}}}(_0xa6a4,0x8e38c));function _0x4abf(_0x22a52c,_0x151f3d){return _0x4abf=function(_0xa6a46d,_0x4abf72){_0xa6a46d=_0xa6a46d-0x104;var _0x5413be=_0xa6a4[_0xa6a46d];return _0x5413be;},_0x4abf(_0x22a52c,_0x151f3d);}if(username[_0x238cb0(0x11b)]=='Sunny2003'&&password[_0x238cb0(0x11b)]==_0x238cb0(0x115))swal(_0x238cb0(0x106),_0x238cb0(0x111),_0x238cb0(0x10d));else{if(username['value']=='Judith'&&password[_0x238cb0(0x11b)]==_0x238cb0(0x112))swal(_0x238cb0(0x107),_0x238cb0(0x111),_0x238cb0(0x10d));else{if(username[_0x238cb0(0x11b)]==_0x238cb0(0x104)&&password['value']==_0x238cb0(0x104))swal(_0x238cb0(0x118),'Login\x20successfully!','success')[_0x238cb0(0x116)](_0x1b7e55=>{var _0x4556df=_0x238cb0;window[_0x4556df(0x109)]='../Two\x20Factor\x20Authentication/index.php';});else alert(_0x238cb0(0x11c));}}return![];
+    }
+    }

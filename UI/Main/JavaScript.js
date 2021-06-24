2b43b546eaaa5fc383a16fe68390d228f57680e0
Kitchannel2010 <84881541+Kitchannel2010@users.noreply.github.com>
2021-06-25 07:22:36 +0800

    Add files via upload

---
+++ UI/Main/JavaScript.js
@@ -0,0 +1,28 @@
+﻿function logout() {
+Swal.fire({
+  title: 'Are you sure?',
+  text: "You are going to logout! Are you sure about that?",
+  icon: 'warning',
+  confirmButtonColor: '#3085d6',
+    showCancelButton: true,
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes!'
+}).then((result) => {
+  if (result.isConfirmed) {
+              Swal.fire(
+                'Logout successfully!',
+                '',
+              'success'
+      ).then((result) => {
+            window.location="../../Login/HtmlPage.html"
+     })    
+  }
+})
+
+}
+function star_rating() {
+  window.location="../star rating/star rating.html"
+}
+function update() {
+  window.location = "../browser update/index.html"
+}

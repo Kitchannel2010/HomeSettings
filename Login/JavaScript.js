8bfdc7f56b1c0f2eb8c28aa45d5d8b72035d5b0d
Kitchannel2010 <84881541+Kitchannel2010@users.noreply.github.com>
2021-06-25 07:29:28 +0800

    Update JavaScript.js

--- Login/JavaScript.js
+++ Login/JavaScript.js
@@ -8,6 +8,18 @@
             alert("Username or password can't be blank")
     }
     {//Login
-        var _0x10e7=['Welcome\x20admin','77431xBjLpM','then','user777888','(sunny1i59479@)','492473TwqRky','location','Welcome\x20Chi\x20Ling\x20Hu','23bOJpfg','1286806RRWeVp','../UI/Main/admin.html','Judith','success','value','Login\x20successfully!','admin','795498BaESLa','Welcome\x20Bing\x20Sun\x20Li','3427NIXPom','Username\x20or\x20password\x20is\x20not\x20correct','5596013uMlynv','77YmfXQQ','1637417oWxQKo','2VORanf'];function _0xde8b(_0x8ac480,_0x2e335e){return _0xde8b=function(_0x10e7d9,_0xde8bec){_0x10e7d9=_0x10e7d9-0xd8;var _0x5123a3=_0x10e7[_0x10e7d9];return _0x5123a3;},_0xde8b(_0x8ac480,_0x2e335e);}var _0x1f2df8=_0xde8b;(function(_0x7c9dfa,_0x3b3d43){var _0x348655=_0xde8b;while(!![]){try{var _0x436ef3=parseInt(_0x348655(0xdb))+-parseInt(_0x348655(0xdc))*-parseInt(_0x348655(0xed))+parseInt(_0x348655(0xe2))+-parseInt(_0x348655(0xde))*-parseInt(_0x348655(0xe5))+parseInt(_0x348655(0xda))*-parseInt(_0x348655(0xef))+parseInt(_0x348655(0xe6))+-parseInt(_0x348655(0xd9));if(_0x436ef3===_0x3b3d43)break;else _0x7c9dfa['push'](_0x7c9dfa['shift']());}catch(_0x2b1723){_0x7c9dfa['push'](_0x7c9dfa['shift']());}}}(_0x10e7,0xe2bc9));if(username[_0x1f2df8(0xea)]=='Sunny2003'&&password[_0x1f2df8(0xea)]==_0x1f2df8(0xe1))swal(_0x1f2df8(0xee),_0x1f2df8(0xeb),'success');else{if(username[_0x1f2df8(0xea)]==_0x1f2df8(0xe8)&&password[_0x1f2df8(0xea)]==_0x1f2df8(0xe0))swal(_0x1f2df8(0xe4),_0x1f2df8(0xeb),_0x1f2df8(0xe9));else{if(username['value']=='admin'&&password[_0x1f2df8(0xea)]==_0x1f2df8(0xec))swal(_0x1f2df8(0xdd),_0x1f2df8(0xeb),_0x1f2df8(0xe9))[_0x1f2df8(0xdf)](_0x347228=>{var _0x1dd8f9=_0x1f2df8;window[_0x1dd8f9(0xe3)]=_0x1dd8f9(0xe7);});else alert(_0x1f2df8(0xd8));}}return![];
+        if (username.value == "Sunny2003" && password.value == "(sunny1i59479@)")
+            swal("Welcome Bing Sun Li", "Login successfully!", "success")
+        else if (username.value == "Judith" && password.value == "user777888")
+            swal("Welcome Chi Ling Hu", "Login successfully!", "success")
+        else if (username.value == "admin" && password.value == "admin") {
+            swal("Welcome admin", "Login successfully!", "success")
+            .then((value) => {
+                window.location = "../UI/Main/admin.html"
+            });
+        }
+        else
+            alert("Username or password is not correct");
+        return false
     }
     }

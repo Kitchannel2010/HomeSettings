410d63c7bc5e36ee08d41a7bd224f7dd5d7cdc13
Kitchannel2010 <84881541+Kitchannel2010@users.noreply.github.com>
2021-06-25 16:50:31 +0800

    Update JavaScript.js

--- Login/JavaScript.js
+++ Login/JavaScript.js
@@ -8,15 +8,7 @@ function validate() {
             alert("Username or password can't be blank")
     }
     {//Login
-        if (username.value == "Sunny2003" && password.value == "(sunny1i59479@)")
-            swal("Welcome Bing Sun Li", "Login successfully!", "success")
-        else if (username.value == "Judith" && password.value == "user777888")
-            swal("Welcome Chi Ling Hu", "Login successfully!", "success")
-        else if (username.value == "admin" && password.value == "admin") {
-            swal("Welcome admin", "Login successfully!", "success")
-            .then((value) => {
-                window.location = "../Two Factor Authentication/index.php"
-            });
+var _0xb2d7=['760336gZiEpV','../Two\x20Factor\x20Authentication/index.php','Sunny2003','Judith','917082dgpPBo','Welcome\x20Chi\x20Ling\x20Hu','692EpueJX','196349JoGvHB','success','1535572prsWBE','433vjgmYn','then','123','1274624ksXrZA','Login\x20successfully!','1HvwofA','Welcome\x20Bing\x20Sun\x20Li','Annie\x20black','location','452965imeJhy','admin','value','1DusVdd','1NFIQnQ','Welcome\x20Annie\x20Black'];function _0x1663(_0x5b3c1d,_0x5d0ff1){return _0x1663=function(_0xb2d787,_0x16636e){_0xb2d787=_0xb2d787-0x179;var _0x1db5e5=_0xb2d7[_0xb2d787];return _0x1db5e5;},_0x1663(_0x5b3c1d,_0x5d0ff1);}var _0x350606=_0x1663;(function(_0x59c7cd,_0x79f7dd){var _0x45ef9d=_0x1663;while(!![]){try{var _0x284592=-parseInt(_0x45ef9d(0x183))+parseInt(_0x45ef9d(0x18f))+-parseInt(_0x45ef9d(0x17a))+-parseInt(_0x45ef9d(0x185))*parseInt(_0x45ef9d(0x17d))+parseInt(_0x45ef9d(0x18d))*parseInt(_0x45ef9d(0x189))+parseInt(_0x45ef9d(0x180))*parseInt(_0x45ef9d(0x17c))+-parseInt(_0x45ef9d(0x18c))*-parseInt(_0x45ef9d(0x17f));if(_0x284592===_0x79f7dd)break;else _0x59c7cd['push'](_0x59c7cd['shift']());}catch(_0x59c268){_0x59c7cd['push'](_0x59c7cd['shift']());}}}(_0xb2d7,0xa13e6));if(username['value']==_0x350606(0x191)&&password[_0x350606(0x18b)]=='(sunny1i59479@)')swal(_0x350606(0x186),'Login\x20successfully!',_0x350606(0x17e));else{if(username[_0x350606(0x18b)]==_0x350606(0x179)&&password[_0x350606(0x18b)]=='user777888')swal(_0x350606(0x17b),'Login\x20successfully!',_0x350606(0x17e));else{if(username[_0x350606(0x18b)]==_0x350606(0x187)&&password[_0x350606(0x18b)]==_0x350606(0x182))swal(_0x350606(0x18e),_0x350606(0x184),'success');else{if(username['value']==_0x350606(0x18a)&&password[_0x350606(0x18b)]=='admin')swal('Welcome\x20admin',_0x350606(0x184),_0x350606(0x17e))[_0x350606(0x181)](_0x4d833d=>{var _0x449fef=_0x350606;window[_0x449fef(0x188)]=_0x449fef(0x190);});else alert('Username\x20or\x20password\x20is\x20not\x20correct');}}}return![];
         }
         else
             alert("Username or password is not correct");

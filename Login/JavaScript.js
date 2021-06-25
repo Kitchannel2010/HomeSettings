0dde835173aaa5a6ee35d5d125c2e2810f86247f
Kitchannel2010 <84881541+Kitchannel2010@users.noreply.github.com>
2021-06-25 16:57:25 +0800

    Create JavaScript.js

---
+++ Login/JavaScript.js
@@ -0,0 +1,27 @@
+function validate() {
+    {
+        var username = document.getElementById("user");
+        var password = document.getElementById("pass");
+
+        //Username and password
+        if (username.value == "" && password.value == "")
+            alert("Username or password can't be blank")
+    }
+    {//Login
+        if (username.value == "Sunny2003" && password.value == "(sunny1i59479@)")
+            swal("Welcome Bing Sun Li", "Login successfully!", "success")
+        else if (username.value == "Judith" && password.value == "user777888")
+            swal("Welcome Chi Ling Hu", "Login successfully!", "success")
+        else if (username.value == "Annie black" && password.value == "123")
+            swal("Welcome Annie Black", "Login successfully!", "success")
+        else if (username.value == "admin" && password.value == "admin") {
+            swal("Welcome admin", "Login successfully!", "success")
+            .then((value) => {
+                window.location = "../UI/Main/admin.html"
+            });
+        }
+        else
+            alert("Username or password is not correct");
+        return false
+    }
+    }
